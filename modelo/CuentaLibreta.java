d4de654f44f7637eab45364f93b2884ca7532cc8
Juanherrera117 <relujuca@gmail.com>
2022-12-01 08:33:57 -0500

    -

--- modelo/CuentaLibreta.java
+++ modelo/CuentaLibreta.java
@@ -2,8 +2,20 @@ package modelo;
 
 public class CuentaLibreta extends Cuenta
 {
+    double saldo;
+
     public CuentaLibreta(int cuenta)
     {
         super(cuenta);
-    }         
+    }       
+
+    public void depositar(double cantidad)
+    {
+        saldo = saldo + cantidad;
+    }
+
+    public void retirar(double cantidad)
+    {
+        saldo = saldo - cantidad;
+    }
 }
